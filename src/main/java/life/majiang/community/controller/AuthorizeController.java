0f2dccb4b48e06ece17918fe9f1b8423f2543598
brged <asksa123@163.com>
2022-04-12 22:48:45 +0800

    添加日志

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import life.majiang.community.dto.GithubUser;
 import life.majiang.community.model.User;
 import life.majiang.community.provider.GithubProvider;
 import life.majiang.community.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
+@Slf4j
 public class AuthorizeController {
 
     @Autowired
@@ -67,6 +69,8 @@ public class AuthorizeController {
             servletResponse.addCookie(new Cookie("token", user.getToken()));
             return "redirect:/";
         } else {
+            //登录失败
+            log.error("callback get github error, {}", userInfo);
             return "redirect:/";
         }
     }
